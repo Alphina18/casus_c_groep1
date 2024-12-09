9d1aba17c598d490b79be859676b1204e5ffd6d1
Alphina18 <aimeepruis@outlook.com>
2024-12-09 19:57:05 +0100

    Changed the code in the models file to have an ODE solver and something with finding the best parameters. Will continue doing that and making all the models work with it aswell. In use_case there is code to show it kinda works.

--- models.py
+++ models.py
@@ -4,52 +4,75 @@ from scipy.integrate import solve_ivp
 print("models.py is geladen")
 
 class GrowthModel:  # super class
-    def __init__(self, c, t=None, Vmax=None, Vmin=None, d=None, f=None, V0=None):
-        self.c = c  
-        self.Vmax = Vmax 
-        self.Vmin = Vmin  
-        self.d = d  
-        self.f = f
-        self.V0 = V0
-        self.t = t
+    def __init__(self, model, c=1.0, d=1.0):
+        self.c = c
+        self.d = d
+        self.model = model
 
     def growth_rate(self, V):
         raise NotImplementedError("Deze methode moet worden geïmplementeerd door subklassen.")
     
-    def euler_method(self):
-        steps = 10
-        y = 0.0                             # Beginconditie
-        dt = self.t / steps
-        for step in range(steps):
-            dydt = y + 1.0                  # Differentiaalvergelijking
-            y += dydt * dt
-        return y
+    def euler_method(self, t):
+        steps = 100
+        V = 0                                     # Beginconditie
+        dt = t / steps
+        for _ in range(steps):
+            dVdt = self.model.growth_rate(self, max(V, 1e-10)) # Differentiaalvergelijking
+            V += dVdt * dt
+        return V
+    
+    def euler_method_test(self, t, **params):
+        steps = 1000
+        V = params['V0']                                     # Beginconditie
+        dt = t / steps
+        for _ in range(steps):
+            dVdt = self.model.growth_rate_test(self, V, params['d'] , params['c'] , params['V0'] , params['Vmax'] , params['Vmin'] ) # Differentiaalvergelijking
+            V += dVdt * dt
+        return V
 
-    def rungekutta_method(self):
-        y = self.V0                              # Beginconditie
-        steps = int(abs(self.t) / 0.01) + 1      # Hoeveel tijdstappen van ~0.01 zijn nodig
-        dt = self.t / steps
-        for step in range(steps):
-            # Tijdelijke stappen:
-            dydt1 = a * y + b               # Differentiaalvergelijking stap 1
-            y1 = y + dydt1 * 0.5 * dt
-            dydt2 = a * y1 + b              # Differentiaalvergelijking stap 2
-            y2 = y + dydt2 * 0.5 * dt
-            dydt3 = a * y2 + b              # Differentiaalvergelijking stap 3
-            y3 = y + dydt3 * dt
-            dydt4 = a * y3 + b              # Differentiaalvergelijking stap 4
-            # Definitieve stap:
-            dydt = (dydt1 + 2.0 * dydt2 + 2.0 * dydt3 + dydt4) / 6.0
-            y += dydt * dt
-        return y
+    def mean_squared_error(self, data_ts, data_ys, **params):
+        N = len(data_ts)
+        total = 0.0
+        for i in range(N):
+            error = data_ys[i] - GrowthModel.euler_method_test(self, t=data_ts[i], **params)
+            total += error * error
+        return total / N
 
-    def other_method(self):
-        pass
+    def best_params(self, data_ts, data_ys, params):#
+        print(params)
+        deltas = {key: 1.0 for key in params}
 
+        mse = GrowthModel.mean_squared_error(self, data_ts, data_ys, **params)
+        while max(abs(delta) for delta in deltas.values()) > 1e-8:
+            for key in params:
+                new_params = params.copy()
+                # Probeer de betreffende parameter the verhogen
+                new_params[key] = params[key] + deltas[key]
+                new_mse = GrowthModel.mean_squared_error(self, data_ts, data_ys, **new_params)
+                if new_mse < mse:
+                    params = new_params
+                    mse = new_mse
+                    deltas[key] *= 1.2
+                    continue
+                # Probeer de betreffende parameter the verlagen
+                new_params[key] = params[key] - deltas[key]
+                new_mse = GrowthModel.mean_squared_error(self, data_ts, data_ys, **new_params)
+                if new_mse < mse:
+                    params = new_params
+                    mse = new_mse
+                    deltas[key] *= -1.0
+                    continue
+                # Verklein de stapgrootte
+                deltas[key] *= 0.5
+        return new_params
 
 class LinearGrowth(GrowthModel):
     def growth_rate(self, V):
         return self.c
+
+class LinearGrowth_test(GrowthModel):
+    def growth_rate_test(self, V,d,c,V0, Vmax, Vmin):
+        return c
 
 class ExponentialGrowth(GrowthModel):
     def growth_rate(self, V):
@@ -65,7 +88,7 @@ class ExponentialDecayGrowth(GrowthModel):
 
 class LogisticGrowth(GrowthModel):
     def growth_rate(self, V):
-        return self.c * V * (self.Vmax - V)
+        return self.c * self.V * (self.V_max - self.V)
 
 class MontrollGrowth(GrowthModel):
     def growth_rate(self, V):
@@ -80,13 +103,19 @@ class LinearLimitedGrowth(GrowthModel):
         return self.c * (V / (V + self.d))
 
 class SurfaceLimitedGrowth(GrowthModel):
+    def __init__(self, c, d):
+        super().__init__(c, d)
+
     def growth_rate(self, V):
         return self.c * (V / ((V + self.d) ** (1/3)))
 
 class VonBertalanffyGrowth(GrowthModel):
+    def __init__(self, c, d):
+        super().__init__(c, d)
+
     def growth_rate(self, V):
         return self.c * (V ** (2/3)) - self.d * V
 
 class GompertzGrowth(GrowthModel):
     def growth_rate(self, V):
-        return self.c * V * np.log(self.Vmax / V)
+        return self.c * V * np.log(1.0 / V)
